b9b01648d2407a4396c819144f5fbcaea925eeb2
liz <90375135+liz-cs@users.noreply.github.com>
2022-06-23 15:05:15 -0400

    add claimable helper

--- src/apps/agave/gnosis/agave.claimable.contract-position-fetcher.ts
+++ src/apps/agave/gnosis/agave.claimable.contract-position-fetcher.ts
@@ -1,12 +1,12 @@
 import { Inject } from '@nestjs/common';
 
 import { Register } from '~app-toolkit/decorators';
-import { AaveV2ClaimableContractPositionHelper } from '~apps/aave-v2/helpers/aave-v2.claimable.contract-position-helper';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { ContractPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
 
 import { AGAVE_DEFINITION } from '../agave.definition';
+import { AgaveClaimableContractPositionHelper } from '../helper/agave.claimable.contract-position-helper';
 
 const appId = AGAVE_DEFINITION.id;
 const groupId = AGAVE_DEFINITION.groups.claimable.id;
@@ -15,18 +15,19 @@ const network = Network.GNOSIS_MAINNET;
 @Register.ContractPositionFetcher({ appId, groupId, network })
 export class GnosisAgaveClaimableContractPositionFetcher implements PositionFetcher<ContractPosition> {
   constructor(
-    @Inject(AaveV2ClaimableContractPositionHelper)
-    private readonly aaveV2ClaimableContractPositionHelper: AaveV2ClaimableContractPositionHelper,
+    @Inject(AgaveClaimableContractPositionHelper)
+    private readonly agaveClaimableContractPositionHelper: AgaveClaimableContractPositionHelper,
   ) {}
 
   async getPositions() {
-    return this.aaveV2ClaimableContractPositionHelper.getTokens({
+    return this.agaveClaimableContractPositionHelper.getTokens({
       appId,
       groupId,
       network,
       incentivesControllerAddress: '0xfa255f5104f129b78f477e9a6d050a02f31a5d86',
       protocolDataProviderAddress: '0x24dcbd376db23e4771375092344f5cbea3541fc0',
-      rewardTokenAddress: '0x3a97704a1b25f08aa230ae53b352e2e72ef52843',
+      //rewardTokenAddress: '0x3a97704a1b25f08aa230ae53b352e2e72ef52843',
+      dependencies: [],
     });
   }
 }
